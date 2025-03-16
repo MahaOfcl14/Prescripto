978b505ad12727c91a1ac41883c42d97ee049eb9
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-16 12:10:20 +0530

    Banner Section created and completed successfully

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -9,8 +10,13 @@ const Home = () => {
         <Header/>
         <SpecialityMenu/>
         <TopDoctors/>
+        <Banner/>
     </div>
   )
 }
 
 export default Home
+
+
+
+//bg-[#5f6FFF]
