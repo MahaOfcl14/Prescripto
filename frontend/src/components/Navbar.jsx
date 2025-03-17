9f0781196b192357f3489460226c005554881282
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-17 19:04:17 +0530

    FrontEnd part completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = () => {
       <div className="flex item-center gap-4 ">
         {token ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
+            <img  className="w-8 rounded-full" src={assets.profile_pic} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-         <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+         <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20  hidden group-hover:block ">
             <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p onClick={()=>navigate('/my-profile')} className="hover:text-black cursor-pointer">My Profile</p>
                 <p onClick={()=>navigate('/my-appointments')} className="hover:text-black cursor-pointer">My Appointments</p>
@@ -65,10 +65,14 @@ const Navbar = () => {
             <NavLink  onClick={()=>setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
             <NavLink  onClick={()=>setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
             <NavLink  onClick={()=>setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/my-profile'><p className='px-4 py-2 rounded inline-block'>PROFILE</p></NavLink>
+            <NavLink  onClick={()=>setShowMenu(false)} to='/my-appointments'><p className='px-4 py-2 rounded inline-block'>MY APPOINTMENTS</p></NavLink>
+
 
           </ul>
 
         </div>
+       
       </div>
     </div>
   );
