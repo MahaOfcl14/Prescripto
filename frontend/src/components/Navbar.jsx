8826f9f7699ed6e412adca2c8696ec7553823cf3
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-17 10:18:17 +0530

    All doctors page Ui and Functionality completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img onClick={()=>navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="" />
-      <ul className="hidden md:flex items-start gap-5 font-medium">
+      <ul className="hidden md:flex items-start gap-5 font-medium text-lg">
         <NavLink to="/">
           <li className="py-1">Home</li>
           <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden " />
